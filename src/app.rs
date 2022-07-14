5dfb610265ee03d830c95905b82f6b4981860569
Le Turt <89463679+LeTurt333@users.noreply.github.com>
2022-07-14 04:16:34 -0500

    clean up

--- src/app.rs
+++ src/app.rs
@@ -138,19 +138,19 @@ pub fn encode_decode(user_address: &str) -> String {
     // Error: decode/encode failed - Hits when someone enters a valid CW address but non Juno address (like a cosmos address)
 
     match bech32::decode(user_address) {
-        Err(x) => return format!("Error_1: {:?}", x),
+        Err(x) => return format!("Error: {:?}", x),
         Ok(_) => {
             let (_hrp, data, _variant) = bech32::decode(user_address).unwrap();
 
             match Vec::<u8>::from_base32(&data) {
-                Err(x) => return format!("Error_1: {:?}", x),
+                Err(x) => return format!("Error: {:?}", x),
                 Ok(_) => {
                     let xxx = Vec::<u8>::from_base32(&data).unwrap();
 
                     let yyy = Vec::to_base32(&xxx);
 
                     match bech32::encode("juno", yyy.clone(), bech32::Variant::Bech32) {
-                        Err(x) => return format!("Error_1: {:?}", x),
+                        Err(x) => return format!("Error: {:?}", x),
                         Ok(_) => {
                             let check =
                                 bech32::encode("juno", yyy, bech32::Variant::Bech32).unwrap();
@@ -158,7 +158,7 @@ pub fn encode_decode(user_address: &str) -> String {
                             if check == user_address {
                                 return user_address.to_string();
                             } else {
-                                return "Error_1: Decode/encode failed. Please enter a valid Juno address".to_string();
+                                return "Error: Please enter a valid Juno address".to_string();
                             }
                         }
                     }
@@ -172,12 +172,12 @@ pub fn verify_length(user_address: &str) -> String {
     // Error if length of bytes > 255 (max length of CW bech32 address)
 
     match bech32::decode(user_address) {
-        Err(x) => return format!("Error_2: {:?}", x),
+        Err(x) => return format!("Error: {:?}", x),
         Ok(_) => {
             let (_hrp, data, _variant) = bech32::decode(user_address).unwrap();
 
             if !matches!(data.len(), 1..=MAX_ADDRESS_LENGTH) {
-                return "Error_2: Address is an invalid length".to_string();
+                return "Error: Address is an invalid length".to_string();
             } else {
                 return user_address.to_string();
             }
